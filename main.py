88039a3bf073f616fac5bf590b93adb04d189c92
Giga Samkharadze <gigasamkharadzee@gmail.com>
2024-02-27 01:41:53 +0400

    option selection and page switching handled

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ class MyWindow(QMainWindow):
         super(MyWindow, self).__init__()
         uic.loadUi('ui/main.ui', self)
         self.start_button.clicked.connect(self.start)
+        self.select_button.clicked.connect(self.continue_with_selected_figure)
 
     def start(self):
         self.stackedWidget.setCurrentIndex(1)
+
+    def continue_with_selected_figure(self):
+        selected_radio = self.figure_choice.checkedButton()
+        if selected_radio:
+            current_page = self.figure_choice.checkedButton().text().lower()
+            self.stackedWidget.setCurrentWidget(
+                getattr(self, f'{current_page}_page', self.figure_select_page)
+            )
 
 
 app = QApplication(sys.argv)
 window = MyWindow()
 window.show()
 sys.exit(app.exec_())
-
