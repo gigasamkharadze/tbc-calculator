c1663721da4e9bbeaa34b173f75a0df05c3ebe18
Giga Samkharadze <gigasamkharadzee@gmail.com>
2024-02-27 16:46:26 +0400

    page for calculating length and area of the circle

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         uic.loadUi('ui/main.ui', self)
-        self.start_button.clicked.connect(self.start)
+        self.start_button.clicked.connect(self.to_start)
         self.select_button.clicked.connect(self.continue_with_selected_figure)
+        self.radius_input.valueChanged.connect(lambda value: self.update_circle_output(value))
+        self.home_button.clicked.connect(self.to_home)
+        self.next_button_circle.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(3))
 
-    def start(self):
+    def to_home(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+    def to_start(self):
         self.stackedWidget.setCurrentIndex(1)
 
     def continue_with_selected_figure(self):
@@ -21,6 +27,12 @@ class MyWindow(QMainWindow):
             self.stackedWidget.setCurrentWidget(
                 getattr(self, f'{current_page}_page', self.figure_select_page)
             )
+
+    def update_circle_output(self, value):
+        area = 3.14 * value ** 2
+        length = 2 * 3.14 * value
+        self.circle_area_output.setText(f'{area:.2f}')
+        self.circle_length_output.setText(f'{length:.2f}')
 
 
 app = QApplication(sys.argv)
